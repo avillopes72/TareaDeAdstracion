b19f721951e2c937ead3b656e36ffea5c037e382
avillopes72 <avillopes72@gmail.com>
2026-06-02 18:17:55 -0500

    hoja2

--- src/main/java/DaoImplement/DaoImplementMoto.java
+++ src/main/java/DaoImplement/DaoImplementMoto.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Dao;
+package DaoImplement;
 
 /**
  *
  * @author HP
  */
-public class DaoImplement {
+public class DaoImplementMoto {
     
 }
--- src/main/java/lop/tallerl/Vehiculo.java
+++ src/main/java/lop/tallerl/Vehiculo.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Dao;
+package lop.tallerl;
 
 /**
  *
  * @author HP
  */
-public class DaoInterfaz {
+public class Vehiculo {
     
 }
